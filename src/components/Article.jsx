47350b84a0d1ea21437f46f7f98542dce7d55800
phil-pickering <hello@philpickering.com>
2024-06-20 08:58:20 +0100

    allows users to see comments

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../utils/api";
+import { patchLikes } from "../utils/api";
 import Header from "./Header";
 import Comments from "./Comments";
 
 export default function Article() {
   const [article, setArticle] = useState([]);
+  const [likes, setLikes] = useState(article.votes);
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -15,20 +17,37 @@ export default function Article() {
   }, []);
 
   let formattedDate = "";
-
   if (article.created_at) {
     formattedDate = article.created_at.slice(0, 10);
-  } else
+  } else {
     return (
       <>
         <Header />
         <p>Loading article...</p>
       </>
     );
+  }
+
+  function handleLike() {
+    setLikes(likes + 1);
+
+    patchLikes(article_id).then((article) => {
+      setArticle((article) => ({
+        ...article,
+        votes: article.votes + 1,
+      }));
+    });
+  }
+
+  function handleAddComment() {
+    const addCommentDlg = document.getElementById("addCommentDlg");
+    addCommentDlg.showModal();
+  }
 
   return (
     <>
       <Header />
+
       <article>
         <h2>{article.title}</h2>
         <img src={article.article_img_url} alt="" height="250" />
@@ -43,8 +62,19 @@ export default function Article() {
           <dd>{article.votes}</dd>
         </dl>
         <p>{article.body}</p>
-        <button>Like ❤️</button>
-        <button>Add comment 💬</button>
+
+        <button onClick={handleLike}>Like &nbsp; ❤️</button>
+
+        <button onClick={handleAddComment}>Add comment &nbsp; 💬</button>
+
+        <dialog id="addCommentDlg">
+          <form method="dialog">
+            <textarea rows="4" cols="50"></textarea>
+            <p>
+              <button>Post comment &nbsp; 📮</button>
+            </p>
+          </form>
+        </dialog>
       </article>
       <Comments />
     </>
